630270edcc62c0d338cb85a8e52158ddb0540598
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-24 19:30:21 +0200

    first commit

---
+++ Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while(contador<101){
+            int multiplos = 5*contador;
+            contador++;
+            System.out.println(multiplos);
+        }
+    }
+}
