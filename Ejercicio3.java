40cc232c3506e9118be50042fe2853b2856cd589
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-24 19:30:26 +0200

    first commit

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int contador = 0;
+        do{
+            int multiplos = 5*contador;
+            contador++;
+            System.out.println(multiplos);
+        }while(contador<101);
+    }
+}
