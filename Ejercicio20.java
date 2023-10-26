9f9e633cdd133a807ccd3bf9173576b72c53c4c9
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-26 23:12:43 +0200

    first commit

---
+++ Ejercicio20.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca la altura de la piramide: ");
+        int alturaPiramide = sc.nextInt();
+        for (int i=0 ;i<alturaPiramide;i++){
+            for (int j=0;j<=alturaPiramide-i;j++) {
+                System.out.print(" ");
+                }
+                
+                for(int k = 0;k<=i*2;k++){
+                    if(k==0 || k==i*2 || i==alturaPiramide-1){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+        
+            }
+                
+                System.out.println("");
+                }
+        sc.close();
+    }
+}
