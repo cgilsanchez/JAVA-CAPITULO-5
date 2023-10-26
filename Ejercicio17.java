1f856dde4ced4749ebbbe451e6c14cda72f67809
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-26 23:12:24 +0200

    first commit

---
+++ Ejercicio17.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un numero entero positivo: ");
+        int n = sc.nextInt();
+        int suma = 0;
+            if(n>0){
+            for (int i=1;i<=100;i++){
+                suma= suma+n;
+                System.out.println(suma);
+                n++;
+            }
+        }else{
+            System.out.println("Introdizca un numero positivo");
+        }
+        sc.close();
+    }
+}
