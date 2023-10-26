8dfe9774d9792bb2cb50b219e81f7a099aad2d2c
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-26 23:12:18 +0200

    first commit

---
+++ Ejercicio16.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca un numero por teclado para saber si es primo o no");
+        int n = sc.nextInt();
+        for(int i = 1;i<=1;i++){
+            if(n%2==0){
+                System.out.println(" No es un numero primo");
+
+            }else{
+                System.out.println("El numero es primo");
+            }
+        }
+        sc.close();
+    }
+}
