7ef2b156e522ec68d3f5ef59e24fa24bda11f7d1
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-28 14:34:15 +0200

    first commit

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -17,6 +17,7 @@ public class Ejercicio20 {
                         System.out.print(" ");
                     }
         
+                    
             }
                 
                 System.out.println("");
