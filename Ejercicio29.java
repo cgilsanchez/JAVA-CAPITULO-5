07e7f637f329bb4659d78262070f0c29324e1e64
cgilsanchez <cgilsanchez.02@gmail.com>
2023-10-30 11:04:44 +0100

    first commit

---
+++ Ejercicio29.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio29 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un primer numero: ");
+        int num1 = sc.nextInt();
+        System.out.print("Introduzca el segundo numero: ");
+        int num2=sc.nextInt();
+            
+        for(int i = num1;i>=1;i--){
+                if(i%2!=0){
+                    System.out.println(i);
+                }
+            }
+        sc.close();
+    }
+}
