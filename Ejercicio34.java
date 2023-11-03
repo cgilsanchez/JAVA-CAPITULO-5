61733ee396db94770da7a26c415d965cc86f4077
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-03 12:00:46 +0100

    first commit

---
+++ Ejercicio34.java
@@ -0,0 +1,8 @@
+public class Ejercicio34 {
+    public static void main(String[] args) {
+        System.out.print("Por favor , introduzca un numero: ");
+        long n = Long.parseLong(System.console().readLine());
+        System.out.print("Introduzca otro numero: ");
+        long n1 = Long.parseLong(System.console().readLine());
+    }
+}
