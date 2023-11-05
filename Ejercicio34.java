ed71dfcc9718c0c0119cd73a89f5479fcc505435
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-05 13:16:09 +0100

    first commit

--- Ejercicio34.java
+++ Ejercicio34.java
@@ -4,5 +4,29 @@ public class Ejercicio34 {
         long n = Long.parseLong(System.console().readLine());
         System.out.print("Introduzca otro numero: ");
         long n1 = Long.parseLong(System.console().readLine());
+        long volteado = 0;
+        long volteado1 = 0;
+        while (n>0) {
+            volteado = volteado*10+n%10;
+            n/=10;
+        }
+        while (n1>0){
+            volteado1 = volteado1*10+n1%10;
+            n1/=10;
+        }
+        while (volteado>0 && volteado1>0) {
+            if ((volteado%10)%2==0 && (volteado1%10)%2==0) {
+                System.out.print("El numero formado por los digitos pares es ");
+                System.out.print(volteado%10);
+                System.out.print(volteado1%10);
+                System.out.println();
+            }else if ((volteado%10)%2!=0 && (volteado1%10)%2!=0) {
+                System.out.print("El numero formado por los digitos impares es ");
+                System.out.print(volteado%10);
+                System.out.print(volteado1%10);
+            }
+            volteado/=10;
+            volteado1/=10;
+        }
     }
 }
