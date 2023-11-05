a8ee804ed38cada2d77e39e6b0e289be9e0a25b8
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-05 13:16:45 +0100

    first commit

--- Ejercicio39.java
+++ Ejercicio39.java
@@ -1,3 +1,11 @@
 public class Ejercicio39 {
-    
+    public static void main(String[] args) {
+        System.out.print("Por favor, introduzca un numero entero positivo: ");
+        int n = Integer.parseInt(System.console().readLine());
+        int operacion = 1;
+        for (int i = 1; i <= n; i++) {
+            operacion=operacion*i;
+            System.out.println(i +"! = "+ operacion);
+        }
+    }
 }
