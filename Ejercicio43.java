07a79c1815daa2d5ff1c5227f127f0a333a7612d
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-06 09:27:23 +0100

    first commit

--- Ejercicio43.java
+++ Ejercicio43.java
@@ -1,3 +1,7 @@
 public class Ejercicio43 {
-    
+    public static void main(String[] args) {
+        System.out.print("Por favor, introduzca un numero entero positivo: ");
+        long n = Long.parseLong(System.console().readLine());
+        
+    }
 }
