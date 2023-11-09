9e93546bfaf9ceb720a4e62b2f26c20b22f3ea64
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-09 09:24:51 +0100

    first commit

--- Ejercicio35.java
+++ Ejercicio35.java
@@ -1,3 +1,11 @@
 public class Ejercicio35 {
-    
+    public static void main(String[] args) {
+        System.out.println("Por favor, introduzca la altura de la X: ");
+        int altura = Integer.parseInt(System.console().readLine());
+        if (altura%2!=0 && altura>3) {
+            System.out.println();
+        }else{
+            System.out.println("La altura debe ser impar y mayor a 3");
+        }
+    }
 }
