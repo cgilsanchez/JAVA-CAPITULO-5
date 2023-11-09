d3ad253f7445d851450d33f0825a0ef3cc41e957
cgilsanchez <cgilsanchez.02@gmail.com>
2023-11-09 09:25:10 +0100

    first commit

--- Ejercicio48.java
+++ Ejercicio48.java
@@ -1,3 +1,22 @@
 public class Ejercicio48 {
-    
+    public static void main(String[] args) {
+        System.out.print("Introduzca un numero entero: ");
+        long n = Long.parseLong(System.console().readLine());
+        long volteado = 0;
+        
+        
+        while (n>0) {
+            volteado = volteado*10 + n%10;
+            n/= 10;
+            for (int i = 0; i < 10; i++) {
+                if (volteado%10 == i) {
+                    System.out.print(volteado%10 + " ");
+                }else{
+                    System.out.print(i+ " ");
+                }
+            }
+            System.out.print("Los digitos que aparecen en el numero: ");
+            System.out.print("Dígitos que no aparecen:");
+        }
+    }
 }
